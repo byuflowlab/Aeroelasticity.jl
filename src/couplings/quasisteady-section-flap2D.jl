52bb7b28fc95d27f862edfa02ca94ba3f0723c26
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2021-07-29 16:48:27 -0600

    introduce flap2D

---
+++ src/couplings/quasisteady-section-flap2D.jl
@@ -0,0 +1,152 @@
+"""
+    couple_models(aero::Steady, stru::TypicalSection, flap::Flap2D)
+
+Create an aerostructural model using a steady aerodynamics model, a two-degree
+of freedom typical section model, and a linear steady-state control surface
+model.  This model introduces the freestream velocity ``U_\\infty``, air density
+``\\rho_\\infty``, and flap deflection ``\\delta`` as additional parameters.
+"""
+couple_models(aero::Steady, stru::TypicalSection, flap::Flap2D) = (aero, stru, flap)
+
+"""
+    couple_models(aero::QuasiSteady, stru::TypicalSection, flap::Flap2D)
+
+Create an aerostructural model using a quasi-steady aerodynamics model, a
+two-degree of freedom typical section model, and a linear steady-state control
+surface model.  This model introduces the freestream velocity ``U_\\infty``, air
+density ``\\rho_\\infty``, and flap deflection ``\\delta`` as additional parameters.
+"""
+couple_models(aero::QuasiSteady, stru::TypicalSection, flap::Flap2D) = (aero, stru, flap)
+
+# --- traits --- #
+inplaceness(::Type{QuasiSteady{0}}, ::Type{TypicalSection}, ::Type{Flap2D}) = OutOfPlace()
+mass_matrix_type(::Type{QuasiSteady{0}}, ::Type{TypicalSection}, ::Type{Flap2D}) = Zeros()
+state_jacobian_type(::Type{QuasiSteady{0}}, ::Type{TypicalSection}, ::Type{Flap2D}) = Nonlinear()
+number_of_parameters(::Type{QuasiSteady{0}}, ::Type{TypicalSection}, ::Type{Flap2D}) = 3
+
+inplaceness(::Type{QuasiSteady{1}}, ::Type{TypicalSection}, ::Type{Flap2D}) = OutOfPlace()
+mass_matrix_type(::Type{QuasiSteady{1}}, ::Type{TypicalSection}, ::Type{Flap2D}) = Zeros()
+state_jacobian_type(::Type{QuasiSteady{1}}, ::Type{TypicalSection}, ::Type{Flap2D}) = Nonlinear()
+number_of_parameters(::Type{QuasiSteady{1}}, ::Type{TypicalSection}, ::Type{Flap2D}) = 3
+
+inplaceness(::Type{QuasiSteady{2}}, ::Type{TypicalSection}, ::Type{Flap2D}) = OutOfPlace()
+mass_matrix_type(::Type{QuasiSteady{2}}, ::Type{TypicalSection}, ::Type{Flap2D}) = Linear()
+state_jacobian_type(::Type{QuasiSteady{2}}, ::Type{TypicalSection}, ::Type{Flap2D}) = Nonlinear()
+number_of_parameters(::Type{QuasiSteady{2}}, ::Type{TypicalSection}, ::Type{Flap2D}) = 3
+
+# --- methods --- #
+
+function get_inputs(aero::QuasiSteady{0}, stru::TypicalSection, flap::Flap2D,
+    q, p, t)
+    # extract state variables
+    h, θ, hdot, θdot = q
+    # extract parameters
+    a, b, a0, α0, kh, kθ, m, Sθ, Iθ, clδ, cdδ, cmδ, U, ρ, δ = p
+    # local freestream velocity components
+    u = U
+    v = U*θ
+    # calculate aerodynamic loads
+    L, M = quasisteady0_loads(a, b, ρ, a0, α0, u, v)
+    # add loads due to flap deflections
+    L += ρ*U^2*b*clδ*δ
+    M += 2*ρ*U^2*b^2*cmδ*δ
+    # return inputs
+    return SVector(L, M)
+end
+
+function get_inputs(aero::QuasiSteady{1}, stru::TypicalSection, flap::Flap2D,
+    q, p, t)
+    # extract state variables
+    h, θ, hdot, θdot = q
+    # extract parameters
+    a, b, a0, α0, kh, kθ, m, Sθ, Iθ, clδ, cdδ, cmδ, U, ρ, δ = p
+    # local freestream velocity components
+    u = U
+    v = U*θ + hdot
+    ω = θdot
+    # calculate aerodynamic loads
+    L, M = quasisteady1_loads(a, b, ρ, a0, α0, u, v, ω)
+    # add loads due to flap deflections
+    L += ρ*U^2*b*clδ*δ
+    M += 2*ρ*U^2*b^2*cmδ*δ
+    # return inputs
+    return SVector(L, M)
+end
+
+function get_inputs(aero::QuasiSteady{2}, stru::TypicalSection, q, p, t)
+    # extract state variables
+    h, θ, hdot, θdot = q
+    # extract aerodynamic, structural, and aerostructural parameters
+    a, b, a0, α0, kh, kθ, m, Sθ, Iθ, clδ, cdδ, cmδ, U, ρ, δ = p
+    # local freestream velocity components
+    u = U
+    v = U*θ + hdot
+    ω = θdot
+    # calculate aerodynamic loads
+    L, M = quasisteady2_state_loads(a, b, ρ, a0, α0, u, v, ω)
+    # add loads due to flap deflections
+    L += ρ*U^2*b*clδ*δ
+    M += 2*ρ*U^2*b^2*cmδ*δ
+    # return inputs
+    return SVector(L, M)
+end
+
+function get_input_mass_matrix(aero::QuasiSteady{2}, stru::TypicalSection, q, p, t)
+    # extract aerodynamic, structural, and aerostructural parameters
+    a, b, a0, α0, kh, kθ, m, Sθ, Iθ, clδ, cdδ, cmδ, U, ρ, δ = p
+    # return jacobian
+    return quasisteady2_mass_matrix(a, b, ρ)
+end
+
+# --- performance overloads --- #
+
+function get_input_state_jacobian(aero::QuasiSteady{0}, stru::TypicalSection, q, p, t)
+    # extract aerodynamic, structural, and aerostructural parameters
+    a, b, a0, α0, kh, kθ, m, Sθ, Iθ, clδ, cdδ, cmδ, U, ρ, δ = p
+    # return jacobian
+    return quasisteady0_jacobian(a, b, ρ, a0, U)
+end
+
+function get_input_state_jacobian(aero::QuasiSteady{1}, stru::TypicalSection, q, p, t)
+    # extract aerodynamic, structural, and aerostructural parameters
+    a, b, a0, α0, kh, kθ, m, Sθ, Iθ, clδ, cdδ, cmδ, U, ρ, δ = p
+    # return jacobian
+    return quasisteady1_jacobian(a, b, ρ, a0, U)
+end
+
+function get_input_state_jacobian(aero::QuasiSteady{2}, stru::TypicalSection, q, p, t)
+    # extract aerodynamic, structural, and aerostructural parameters
+    a, b, a0, α0, kh, kθ, m, Sθ, Iθ, clδ, cdδ, cmδ, U, ρ, δ = p
+    # return jacobian
+    return quasisteady2_jacobian(a, b, ρ, a0, U)
+end
+
+# --- unit testing methods --- #
+
+function get_inputs_from_state_rates(aero::QuasiSteady{0}, stru::TypicalSection,
+    dq, q, p, t)
+
+    return @SVector zeros(2)
+end
+
+function get_inputs_from_state_rates(aero::QuasiSteady{1}, stru::TypicalSection,
+    dq, q, p, t)
+
+    return @SVector zeros(2)
+end
+
+function get_inputs_from_state_rates(aero::QuasiSteady{2}, stru::TypicalSection,
+    dq, q, p, t)
+    # extract state rates
+    dh, dθ, dhdot, dθdot = dq
+    # extract parameters
+    a, b, a0, α0, kh, kθ, m, Sθ, Iθ, clδ, cdδ, cmδ, U, ρ, δ = p
+    # local freestream velocity components
+    udot = 0
+    vdot = dhdot
+    ωdot = dθdot
+    # calculate aerodynamic loads
+    L, M = quasisteady2_rate_loads(a, b, ρ, vdot, ωdot)
+    # return inputs
+    return SVector(L, M)
+end
